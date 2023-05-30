3a244f425a78c72aad7dc6db836f8d0575cd9cee
aorekhov <aorekhov@phoenixit.ru>
2023-05-30 15:57:27 +0300

    First part in spring boot 2.0. The best practics

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,4 @@
+insert into todo values ('7fd921cfd2b64dc7b995633e8209f385','Buy Milk','2018-09-23 15:00:01','2018-09-23 15:00:01',false);
+insert into todo values ('5820a4c2abe74f409da89217bf905a0c','Read a Book','2018-09-02 16:00:01','2018-09-02 16:00:01',false);
+insert into todo values ('a44b6db26aef49e39dlb68622f55c347','Go to Spring One 2018','2018-09-18 12:00:00','2018-09-18 12:00:00',false);
+insert into todo values ('5820a4c68456184513a89217bf905a0c','Listen to music','2019-09-02 18:00:01','2019-09-02 18:00:01',false);
